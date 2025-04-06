3bc39460e314f61482f8290142eba1d14ed83228
Sagnik-Das005 <sagnikdas2005@gmail.com>
2025-04-06 18:11:43 +0530

    Create string_vs_char.cpp
    This  C++ program is to understand the ways we can pass string datatype and char array to functions.

---
+++ string_vs_char.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void getsLength(string txt);
+void getsLength2(char *txt);
+void getsLength3(char txt[]);
+
+
+int main()
+
+{
+    int a;
+    string txt="String Datatype";
+    char txt2[50]="Character Array";
+    
+    getsLength(txt);
+    getsLength(txt2);
+    //getsLength2(txt);          => GIVES ERROR 
+    getsLength2(txt2);
+    //getsLength3(txt);          => GIVES ERROR
+    getsLength3(txt2);
+    return 0;
+}
+void getsLength(string txt)
+{
+    cout<<"Parameter:(string txt) \n";
+    cout<<txt<<endl;
+}
+
+void getsLength2(char *txt)
+{
+    cout<<"Parameter:(char *txt) \n";
+    cout<<txt<<endl;
+}
+void getsLength3(char txt[])
+
+{
+    cout<<"Parameter:(char txt[]) \n";
+    cout<<txt<<endl;
+}
